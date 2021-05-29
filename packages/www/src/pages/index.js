064c4b6069adbf0f6cb2fb935a708e1f0a990a2e
Raghunathji <partabrajp@gmail.com>
2021-05-29 17:08:10 +0500

    netlfiy added
    netlify added

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Heading, Button, Flex } from 'theme-ui';
+import netlifyIdentity from 'netlify-identity-widget';
 
-export default (props) => (
-  <Container>
-    <Flex sx={{ flexDirection: 'column', padding: 3 }}>
-      <Heading as="h1">Todo App</Heading>
-      <Button
-        sx={{ marginTop: 2, color: 'black' }}
-        onClick={() => alert('login')}
-      >
-        Login
-      </Button>
-    </Flex>
-  </Container>
-);
+export default (props) => {
+  useEffect(() => {
+    netlifyIdentity.init({});
+  });
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: 'column', padding: 3 }}>
+        <Heading as="h1">Todo App</Heading>
+        <Button
+          sx={{ marginTop: 2, color: 'black' }}
+          onClick={() => {
+            netlifyIdentity.open();
+          }}
+        >
+          Login
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
